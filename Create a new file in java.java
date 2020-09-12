115ac11700c577d5d6dac5a87f36c9bbcef21ea3
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-09-12 20:19:33 +0530

    Add files via upload

---
+++ Create a new file in java.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class CreateFileUsingFileClass
+{
+   public static void main( String[] args )
+   { 
+      try {
+      File file = new File("C:\\newfile.txt");
+     /*createNewFile() will return true if file gets 
+       * created or if the file is 
+       * already present it would return false
+       */
+             boolean filepresent = file.createNewFile();
+      if (filepresent){
+           System.out.println("File is created successfully");
+      }
+      else{
+           System.out.println("File is already present at the specified  location");
+      }
+     } catch (IOException e) {
+      System.out.println("Exception Occurred:");
+         e.printStackTrace();
+   }
+   }
+}
